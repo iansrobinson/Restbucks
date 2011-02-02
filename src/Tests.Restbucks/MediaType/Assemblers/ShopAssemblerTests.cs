969efb37e12f4d99a58d630bb47a22f207b1691f
iansrobinson <iansrobinson@d29beace-2619-7b4a-91c4-328ad5d2ee20>
2011-02-02 11:51:02 +0000

    Moved shop assembler tests into something that just tests assembler rather than processor and stream.
    git-svn-id: file:///Users/iansrobinson/Development/svn-repos/Restbucks.Procurement@251 d29beace-2619-7b4a-91c4-328ad5d2ee20

---
+++ src/Tests.Restbucks/MediaType/Assemblers/ShopAssemblerTests.cs
@@ -0,0 +1,136 @@
+﻿using System.Linq;
+using System.Xml.Linq;
+using NUnit.Framework;
+using Restbucks.MediaType.Assemblers;
+
+namespace Tests.Restbucks.MediaType.Assemblers
+{
+    [TestFixture]
+    public class ShopAssemblerTests
+    {
+        [Test]
+        public void ShouldAssembleShopFromShopElement()
+        {
+            const string xml = @"<?xml version=""1.0"" encoding=""utf-8""?>
+<shop xmlns=""http://schemas.restbucks.com/shop""/>";
+
+            var shop = new ShopAssembler(XElement.Parse(xml)).AssembleShop();
+
+            Assert.IsNotNull(shop);
+            Assert.IsFalse(shop.HasItems);
+            Assert.IsFalse(shop.HasLinks);
+            Assert.IsFalse(shop.HasForms);
+        }
+
+        [Test]
+        public void ShouldAssembleShopWithLinksFromShopElementWithChildLinkElements()
+        {
+            const string xml = @"<?xml version=""1.0"" encoding=""utf-8""?>
+<shop xmlns:rb=""http://relations.restbucks.com/"" xmlns=""http://schemas.restbucks.com/shop"">
+  <link rel=""rb:rfq prefetch"" href=""/quotes"" />
+  <link rel=""rb:order-form"" type=""application/restbucks+xml"" href=""/order-forms/1234"" />
+</shop>";
+
+            var shop = new ShopAssembler(XElement.Parse(xml)).AssembleShop();
+
+            Assert.IsNotNull(shop);
+            Assert.IsFalse(shop.HasItems);
+            Assert.IsTrue(shop.HasLinks);
+            Assert.IsFalse(shop.HasForms);
+
+            Assert.AreEqual(2, shop.Links.Count());
+
+            var firstLink = shop.Links.First();
+            Assert.AreEqual("rb:rfq", firstLink.Rels.First().SerializableValue);
+            Assert.AreEqual("prefetch", firstLink.Rels.Last().SerializableValue);
+            Assert.AreEqual("/quotes", firstLink.Href.ToString());
+            Assert.IsNull(firstLink.MediaType);
+
+            var secondLink = shop.Links.Last();
+            Assert.AreEqual("rb:order-form", secondLink.Rels.First().SerializableValue);
+            Assert.AreEqual("/order-forms/1234", secondLink.Href.ToString());
+            Assert.AreEqual("application/restbucks+xml", secondLink.MediaType);
+        }
+
+        [Test]
+        public void ShouldAssembleShopWithItemsFromShopElementWithItemsElements()
+        {
+            const string xml = @"<?xml version=""1.0"" encoding=""utf-8""?>
+<shop xmlns=""http://schemas.restbucks.com/shop"">
+  <items>
+    <item>
+      <description>item1</description>
+      <amount measure=""g"">250</amount>
+    </item>
+    <item>
+      <description>item2</description>
+      <amount measure=""g"">500</amount>
+      <price currency=""GBP"">5.50</price>
+    </item>
+  </items>
+</shop>";
+
+            var shop = new ShopAssembler(XElement.Parse(xml)).AssembleShop();
+
+            Assert.IsNotNull(shop);
+            Assert.IsTrue(shop.HasItems);
+            Assert.IsFalse(shop.HasLinks);
+            Assert.IsFalse(shop.HasForms);
+
+            Assert.AreEqual(2, shop.Items.Count());
+
+            var firstItem = shop.Items.First();
+            Assert.AreEqual("g", firstItem.Amount.Measure);
+            Assert.AreEqual(250, firstItem.Amount.Value);
+            Assert.IsNull(firstItem.Cost);
+            Assert.AreEqual("item1", firstItem.Description);
+
+            var secondItem = shop.Items.Last();
+            Assert.AreEqual("g", secondItem.Amount.Measure);
+            Assert.AreEqual(500, secondItem.Amount.Value);
+            Assert.AreEqual("GBP", secondItem.Cost.Currency);
+            Assert.AreEqual(5.50, secondItem.Cost.Value);
+            Assert.AreEqual("item2", secondItem.Description);
+        }
+
+        [Test]
+        public void ShouldAssembleShopWithFormsFromShopElementWithChildFormElements()
+        {
+            const string xml = @"<?xml version=""1.0"" encoding=""utf-8""?>
+<shop xmlns=""http://schemas.restbucks.com/shop"">
+  <model schema=""http://schemas.restbucks.com/shop.xsd"" xmlns=""http://www.w3.org/2002/xforms"">
+    <instance />
+    <submission resource=""/quotes"" method=""post"" mediatype=""application/restbucks+xml"" />
+  </model>
+  <model xmlns=""http://www.w3.org/2002/xforms"">
+    <instance>
+      <shop xmlns=""http://schemas.restbucks.com/shop"" />
+    </instance>
+    <submission resource=""/orders"" method=""put"" mediatype=""application/xml"" />
+  </model>
+</shop>";
+            var shop = new ShopAssembler(XElement.Parse(xml)).AssembleShop();
+
+            Assert.IsNotNull(shop);
+            Assert.IsFalse(shop.HasItems);
+            Assert.IsFalse(shop.HasLinks);
+            Assert.IsTrue(shop.HasForms);
+
+            Assert.AreEqual(2, shop.Forms.Count());
+
+            var firstForm = shop.Forms.First();
+            Assert.AreEqual("http://schemas.restbucks.com/shop.xsd", firstForm.Schema.ToString());
+            Assert.IsNull(firstForm.Instance);
+            Assert.AreEqual("/quotes", firstForm.Resource.ToString());
+            Assert.AreEqual("post", firstForm.Method);
+            Assert.AreEqual("application/restbucks+xml", firstForm.MediaType);
+
+            var secondForm = shop.Forms.Last();
+            Assert.IsNull(secondForm.Schema);
+            Assert.IsNotNull(secondForm.Instance);
+            Assert.AreEqual("/orders", secondForm.Resource.ToString());
+            Assert.AreEqual("put", secondForm.Method);
+            Assert.AreEqual("application/xml", secondForm.MediaType);
+        }
+    }
+}
