026c5d72892f15cea05ada6fa0e0817d7656b24f
iansrobinson <iansrobinson@d29beace-2619-7b4a-91c4-328ad5d2ee20>
2011-02-02 09:20:55 +0000

    Added Prefetch and Click methods to Link.
    git-svn-id: file:///Users/iansrobinson/Development/svn-repos/Restbucks.Procurement@246 d29beace-2619-7b4a-91c4-328ad5d2ee20

--- src/Restbucks.MediaType/Link.cs
+++ src/Restbucks.MediaType/Link.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace Restbucks.MediaType
 {
@@ -9,6 +8,7 @@ namespace Restbucks.MediaType
         private readonly IEnumerable<LinkRelation> rels;
         private readonly string mediaType;
         private readonly Uri href;
+        private Shop instance;
 
         public Link(string mediaType, Uri href, params LinkRelation[] rels)
         {
@@ -34,6 +34,16 @@ namespace Restbucks.MediaType
         public Uri Href
         {
             get { return href; }
+        }
+
+        public void Prefetch(Func<Uri, Shop> client)
+        {
+            instance = client(href);
+        }
+
+        public Shop Click(Func<Uri, Shop> client)
+        {
+            return instance ?? client(href);
         }
     }
 }
---
+++ src/Tests.Restbucks/MediaType/LinkTests.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using NUnit.Framework;
+using Restbucks.MediaType;
+
+namespace Tests.Restbucks.MediaType
+{
+    [TestFixture]
+    public class LinkTests
+    {
+        [Test]
+        public void PrefetchInvokesClientWithLinkHref()
+        {
+            Uri uriParameter = null;
+            Func<Uri, Shop> client = uri =>
+                                         {
+                                             uriParameter = uri;
+                                             return null;
+                                         };
+
+            var href = new Uri("http://localhost/shop/");
+            var link = new Link(href);
+
+            link.Prefetch(client);
+
+            Assert.AreEqual(href, uriParameter);
+        }
+
+        [Test]
+        public void IfLinkHasBeenPrefetchedClickReturnsPrefetchedResource()
+        {
+            var shop = new Shop();
+            
+            var href = new Uri("http://localhost/shop/");
+            var link = new Link(href);
+
+            link.Prefetch(uri => shop);
+            
+            Assert.AreEqual(shop, link.Click(uri => null));
+        }
+
+        [Test]
+        public void IfLinkhasNotBeenPrefetchedClieckFetchesResource()
+        {
+            var shop = new Shop();
+
+            var href = new Uri("http://localhost/shop/");
+            var link = new Link(href);
+
+            Assert.AreEqual(shop, link.Click(uri => shop));
+        }
+
+        [Test]
+        public void ClickInvokesClientWithLinkHref()
+        {
+            Uri uriParameter = null;
+            Func<Uri, Shop> client = uri =>
+            {
+                uriParameter = uri;
+                return null;
+            };
+
+            var href = new Uri("http://localhost/shop/");
+            var link = new Link(href);
+
+            link.Click(client);
+
+            Assert.AreEqual(href, uriParameter);
+        }
+    }
+}
