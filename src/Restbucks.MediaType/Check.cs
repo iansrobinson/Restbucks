0b57a9acd70f0a3f4d64f9404e950e805efc9859
iansrobinson <iansrobinson@d29beace-2619-7b4a-91c4-328ad5d2ee20>
2011-02-04 10:39:03 +0000

    Added missing file.
    git-svn-id: file:///Users/iansrobinson/Development/svn-repos/Restbucks.Procurement@276 d29beace-2619-7b4a-91c4-328ad5d2ee20

---
+++ src/Restbucks.MediaType/Check.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Restbucks.MediaType
+{
+    public static class Check
+    {
+        public static void IsNotNull(object o, string name)
+        {
+            if (o == null)
+            {
+                throw new ArgumentNullException(name);
+            }
+        }
+    }
+}
