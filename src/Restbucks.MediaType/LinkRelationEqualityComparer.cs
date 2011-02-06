2242c29c8f8229c8e9a6597ab9d4e25af9e2c84e
iansrobinson <iansrobinson@d29beace-2619-7b4a-91c4-328ad5d2ee20>
2011-02-06 22:02:52 +0000

    Link relation equality comparisons.
    git-svn-id: file:///Users/iansrobinson/Development/svn-repos/Restbucks.Procurement@301 d29beace-2619-7b4a-91c4-328ad5d2ee20

---
+++ src/Restbucks.MediaType/LinkRelationEqualityComparer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Restbucks.MediaType
+{
+    public class LinkRelationEqualityComparer : IEqualityComparer<LinkRelation>
+    {
+        public static readonly LinkRelationEqualityComparer Instance = new LinkRelationEqualityComparer();
+        
+        private LinkRelationEqualityComparer()
+        {
+        }
+
+        public bool Equals(LinkRelation x, LinkRelation y)
+        {
+            return StringComparer.InvariantCultureIgnoreCase.Equals(x.Value, y.Value);
+        }
+
+        public int GetHashCode(LinkRelation obj)
+        {
+            return StringComparer.InvariantCultureIgnoreCase.GetHashCode(obj.Value);
+        }
+    }
+}
---
+++ src/Tests.Restbucks/MediaType/LinkRelationEqualityComparerTests.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Linq;
+using System.Xml.Linq;
+using NUnit.Framework;
+using Restbucks.MediaType;
+using Restbucks.MediaType.Assemblers;
+
+namespace Tests.Restbucks.MediaType
+{
+    [TestFixture]
+    public class LinkRelationEqualityComparerTests
+    {
+        [Test]
+        public void CanBeUsedInLinqExpressions()
+        {
+            var shop = CreateShop();
+
+            var links = (from l in shop.Links
+                         where l.Rels.Contains(new StringLinkRelation("prefetch"), LinkRelationEqualityComparer.Instance)
+                         select l);
+
+            Assert.AreEqual(2, links.Count());
+            Assert.AreEqual(new Uri("/quotes", UriKind.Relative), links.First().Href);
+            Assert.AreEqual(new Uri("http://iansrobinson.com/help"), links.Last().Href);
+        }
+
+        [Test]
+        public void ShouldBeCaseInsensitive()
+        {
+            var shop = CreateShop();
+
+            var links = (from l in shop.Links
+                         where l.Rels.Contains(new StringLinkRelation("PREFETCH"), LinkRelationEqualityComparer.Instance)
+                         select l);
+
+            Assert.AreEqual(2, links.Count());
+            Assert.AreEqual(new Uri("/quotes", UriKind.Relative), links.First().Href);
+            Assert.AreEqual(new Uri("http://iansrobinson.com/help"), links.Last().Href);
+        }
+
+        [Test]
+        public void ShouldHelpFindStringFormattedUriLinkRelations()
+        {
+            var shop = CreateShop();
+
+            var links = (from l in shop.Links
+                         where l.Rels.Contains(new StringLinkRelation("http://relations.iansrobinson.com/help"), LinkRelationEqualityComparer.Instance)
+                         select l);
+
+            Assert.AreEqual(1, links.Count());
+            Assert.AreEqual(new Uri("http://iansrobinson.com/help"), links.First().Href);
+        }
+
+        private static Shop CreateShop()
+        {
+            const string xml = @"<?xml version=""1.0"" encoding=""utf-8""?>
+<shop xmlns:rb=""http://relations.restbucks.com/"" xmlns=""http://schemas.restbucks.com/shop"">
+  <link rel=""rb:rfq prefetch"" type=""application/xml"" href=""/quotes"" />
+  <link rel=""rb:order-form"" type=""application/restbucks+xml"" href=""/order-forms/1234"" />
+  <link rel=""prefetch http://relations.iansrobinson.com/help"" type=""text/html"" href=""http://iansrobinson.com/help"" />
+</shop>";
+
+            return new ShopAssembler(XElement.Parse(xml), new Uri("http://localhost/")).AssembleShop();
+        }
+    }
+}
