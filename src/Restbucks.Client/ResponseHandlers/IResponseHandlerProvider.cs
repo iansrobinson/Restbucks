53da4edec902ead94fa449423261355fe19107ea
iansrobinson <iansrobinson@d29beace-2619-7b4a-91c4-328ad5d2ee20>
2011-02-24 11:53:49 +0000

    REmoving ResponseHandler provider.
    git-svn-id: file:///Users/iansrobinson/Development/svn-repos/Restbucks.Procurement@352 d29beace-2619-7b4a-91c4-328ad5d2ee20

--- src/Restbucks.Client/ResponseHandlers/IResponseHandlerProvider.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Restbucks.Client.ResponseHandlers
-{
-    public interface IResponseHandlerProvider
-    {
-        IResponseHandler GetFor<T>() where T : IResponseHandler;
-    }
-}
--- src/Tests.Restbucks/Client/States/StartedStateTests.cs
+++ src/Tests.Restbucks/Client/States/StartedStateTests.cs
@@ -1,19 +1,11 @@
 ﻿using System;
-using System.IO;
-using System.Net;
 using System.Net.Http;
-using System.Net.Http.Headers;
 using NUnit.Framework;
 using Restbucks.Client;
-using Restbucks.Client.Formatters;
 using Restbucks.Client.Http;
-using Restbucks.Client.ResponseHandlers;
 using Restbucks.Client.States;
-using Restbucks.MediaType;
-using Rhino.Mocks;
 using Tests.Restbucks.Client.Helpers;
 using Tests.Restbucks.Client.States.Helpers;
-using Tests.Restbucks.MediaType.Helpers;
 
 namespace Tests.Restbucks.Client.States
 {
@@ -75,57 +67,6 @@ namespace Tests.Restbucks.Client.States
             var newState = state.Apply();
 
             Assert.AreEqual(response, PrivateField.GetValue<HttpResponseMessage>("response", newState));
-        }
-
-        private static HttpResponseMessage CreateResponseMessage()
-        {
-            var entity = new ShopBuilder().Build();
-            var stream = new MemoryStream();
-
-            RestbucksMediaTypeFormatter.Instance.WriteToStream(entity, stream);
-            stream.Seek(0, SeekOrigin.Begin);
-
-            var content = new StreamContent(stream);
-            content.Headers.ContentType = new MediaTypeHeaderValue(RestbucksMediaType.Value);
-
-            return new HttpResponseMessage {StatusCode = HttpStatusCode.OK, Content = content};
-        }
-
-        private class StubResponseHandlerProvider : IResponseHandlerProvider
-        {
-            private readonly Type handlerType;
-            private readonly IResponseHandler handler;
-
-            public StubResponseHandlerProvider(Type handlerType, IResponseHandler handler)
-            {
-                this.handlerType = handlerType;
-                this.handler = handler;
-            }
-
-            public IResponseHandler GetFor<T>() where T : IResponseHandler
-            {
-                if (handlerType.Equals(typeof (T)))
-                {
-                    return handler;
-                }
-
-                throw new ArgumentException(string.Format("Expected type [{0}]. Invoked with type [{1}].", handlerType.Name, typeof (T).Name));
-            }
-        }
-
-        private class StubResponseHandler : IResponseHandler
-        {
-            public static readonly IResponseHandler Instance = new StubResponseHandler();
-            public static readonly HttpResponseMessage NewResponse = new HttpResponseMessage();
-
-            private StubResponseHandler()
-            {
-            }
-
-            public HandlerResult Handle(HttpResponseMessage response, ApplicationContext context, IHttpClientProvider clientProvider)
-            {
-                return new HandlerResult(true, NewResponse);
-            }
         }
     }
 }
