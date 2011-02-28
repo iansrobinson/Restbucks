06d782944ee403c79a5fc214b5131b4bbe46c568
iansrobinson <iansrobinson@d29beace-2619-7b4a-91c4-328ad5d2ee20>
2011-02-28 23:37:37 +0000

    Added When clause to my new client
    git-svn-id: file:///Users/iansrobinson/Development/svn-repos/Restbucks.Procurement@378 d29beace-2619-7b4a-91c4-328ad5d2ee20

---
+++ src/Restbucks.NewClient/When.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Net.Http;
+
+namespace Restbucks.NewClient
+{
+    public class When : IExecuteAction, IReturnState
+    {
+        private readonly ICondition condition;
+        private IAction action;
+
+        public static IExecuteAction IsTrue<T>() where T : ICondition, new()
+        {
+            return new When(new T());
+        }
+
+        private When(ICondition condition)
+        {
+            this.condition = condition;
+        }
+
+        public IReturnState ExecuteAction(IAction action)
+        {
+            this.action = action;
+            return this;
+        }
+
+        public IRule ReturnState(Func<HttpResponseMessage, IState> createState)
+        {
+            return new Rule(condition, action, new StateFactory(createState));
+        }
+
+        private class StateFactory : IStateFactory
+        {
+            private readonly Func<HttpResponseMessage, IState> createState;
+
+            public StateFactory(Func<HttpResponseMessage, IState> createState)
+            {
+                this.createState = createState;
+            }
+
+            public IState Create(HttpResponseMessage response)
+            {
+                return createState(response);
+            }
+        }
+    }
+
+    public interface IExecuteAction
+    {
+        IReturnState ExecuteAction(IAction action);
+    }
+
+    public interface IReturnState
+    {
+        IRule ReturnState(Func<HttpResponseMessage, IState> createState);
+    }
+}
