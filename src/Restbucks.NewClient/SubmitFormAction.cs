32866e9cfbe6290d39241c3c2d0cb4ad6356a11e
iansrobinson <iansrobinson@d29beace-2619-7b4a-91c4-328ad5d2ee20>
2011-03-01 15:33:12 +0000

    SubmitFormAction now submits form.
    git-svn-id: file:///Users/iansrobinson/Development/svn-repos/Restbucks.Procurement@380 d29beace-2619-7b4a-91c4-328ad5d2ee20

--- src/Restbucks.NewClient/SubmitFormAction.cs
+++ src/Restbucks.NewClient/SubmitFormAction.cs
@@ -1,4 +1,6 @@
-﻿using System.Net.Http;
+﻿using System.Linq;
+using System.Net.Http;
+using Microsoft.Net.Http;
 
 namespace Restbucks.NewClient
 {
@@ -6,16 +8,24 @@ namespace Restbucks.NewClient
     {
         private readonly IFormInfo formInfo;
         private readonly HttpClient client;
+        private readonly object formData;
+        private readonly IContentFormatter[] formatters;
 
-        public SubmitFormAction(IFormInfo formInfo, HttpClient client)
+        public SubmitFormAction(IFormInfo formInfo, HttpClient client, object formData, params IContentFormatter[] formatters)
         {
             this.formInfo = formInfo;
             this.client = client;
+            this.formData = formData;
+            this.formatters = formatters;
         }
 
         public HttpResponseMessage Execute()
         {
-            var content = new StringContent("content");
+            var formatter = (from f in formatters
+                             where f.SupportedMediaTypes.Contains(formInfo.ContentType)
+                             select f).FirstOrDefault();
+
+            var content = formData.ToContent(formatter);
             content.Headers.ContentType = formInfo.ContentType;
 
             return client.Send(new HttpRequestMessage
--- src/Tests.Restbucks/NewClient/SubmitFormActionTests.cs
+++ src/Tests.Restbucks/NewClient/SubmitFormActionTests.cs
@@ -1,7 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using Microsoft.Net.Http;
 using NUnit.Framework;
+using Restbucks.Client.Formatters;
+using Restbucks.MediaType;
 using Restbucks.NewClient;
 using Rhino.Mocks;
 using Tests.Restbucks.Client.Helpers;
@@ -11,46 +17,69 @@ namespace Tests.Restbucks.NewClient
     [TestFixture]
     public class SubmitFormActionTests
     {
+        private static readonly IFormInfo FormInfo = CreateFormInfo();
+        private static readonly Shop FormData = CreateEntityBody();
+        private static readonly IContentFormatter[] Formatters = GetFormatters();
+
         [Test]
         public void ShouldSubmitFormToSuppliedResourceUri()
         {
-            var formInfo = CreateFormInfo();
-
             var mockEndpoint = new MockEndpoint(new HttpResponseMessage());
             var client = new HttpClient {Channel = mockEndpoint};
 
-            var action = new SubmitFormAction(formInfo, client);
+            var action = new SubmitFormAction(FormInfo, client, FormData, Formatters);
             action.Execute();
 
-            Assert.AreEqual(formInfo.ResourceUri, mockEndpoint.ReceivedRequest.RequestUri);
+            Assert.AreEqual(FormInfo.ResourceUri, mockEndpoint.ReceivedRequest.RequestUri);
         }
 
         [Test]
         public void ShouldSubmitFormWithSuppliedMethod()
         {
-            var formInfo = CreateFormInfo();
-
             var mockEndpoint = new MockEndpoint(new HttpResponseMessage());
-            var client = new HttpClient { Channel = mockEndpoint };
+            var client = new HttpClient {Channel = mockEndpoint};
 
-            var action = new SubmitFormAction(formInfo, client);
+            var action = new SubmitFormAction(FormInfo, client, FormData, Formatters);
             action.Execute();
 
-            Assert.AreEqual(formInfo.Method, mockEndpoint.ReceivedRequest.Method);
+            Assert.AreEqual(FormInfo.Method, mockEndpoint.ReceivedRequest.Method);
         }
 
         [Test]
         public void ShouldSubmitFormWithSuppliedContentType()
         {
-            var formInfo = CreateFormInfo();
-
             var mockEndpoint = new MockEndpoint(new HttpResponseMessage());
-            var client = new HttpClient { Channel = mockEndpoint };
+            var client = new HttpClient {Channel = mockEndpoint};
 
-            var action = new SubmitFormAction(formInfo, client);
+            var action = new SubmitFormAction(FormInfo, client, FormData, Formatters);
             action.Execute();
 
-            Assert.AreEqual(formInfo.ContentType, mockEndpoint.ReceivedRequest.Content.Headers.ContentType);
+            Assert.AreEqual(FormInfo.ContentType, mockEndpoint.ReceivedRequest.Content.Headers.ContentType);
+        }
+
+        [Test]
+        public void ShouldSubmitFormWithSuppliedContent()
+        {
+            var mockEndpoint = new MockEndpoint(new HttpResponseMessage());
+            var client = new HttpClient {Channel = mockEndpoint};
+
+            var action = new SubmitFormAction(FormInfo, client, FormData, Formatters);
+            action.Execute();
+
+            var receivedFormData = mockEndpoint.ReceivedRequest.Content.ReadAsObject<Shop>(RestbucksMediaTypeFormatter.Instance);
+
+            Assert.AreEqual(FormData.Items.Count(), receivedFormData.Items.Count());
+            Assert.AreEqual(FormData.Items.First().Description, receivedFormData.Items.First().Description);
+        }
+
+        private static IContentFormatter[] GetFormatters()
+        {
+            return new IContentFormatter[] {new DummyFormatter(), new RestbucksFormatter() };
+        }
+
+        private static Shop CreateEntityBody()
+        {
+            return new ShopBuilder(null).AddItem(new Item("coffee", new Amount("g", 250))).Build();
         }
 
         private static IFormInfo CreateFormInfo()
@@ -58,8 +87,55 @@ namespace Tests.Restbucks.NewClient
             var formInfo = MockRepository.GenerateStub<IFormInfo>();
             formInfo.Expect(f => f.ResourceUri).Return(new Uri("http://restbucks.com/orders"));
             formInfo.Expect(f => f.Method).Return(HttpMethod.Post);
-            formInfo.Expect(f => f.ContentType).Return(new MediaTypeHeaderValue("application/atom+xml"));
+            formInfo.Expect(f => f.ContentType).Return(new MediaTypeHeaderValue(RestbucksMediaType.Value));
             return formInfo;
+        }
+
+        private class RestbucksFormatter : IContentFormatter
+        {
+            public void WriteToStream(object instance, Stream stream)
+            {
+                RestbucksMediaTypeFormatter.Instance.WriteToStream(instance, stream);
+            }
+
+            public object ReadFromStream(Stream stream)
+            {
+                return RestbucksMediaTypeFormatter.Instance.ReadFromStream(stream);
+            }
+
+            public IEnumerable<MediaTypeHeaderValue> SupportedMediaTypes
+            {
+                get
+                {
+                    return new[] {new MediaTypeHeaderValue("application/restbucks+xml")}
+                        .Concat(RestbucksMediaTypeFormatter.Instance.SupportedMediaTypes);
+                }
+            }
+        }
+
+        private class DummyFormatter : IContentFormatter
+        {
+            public void WriteToStream(object instance, Stream stream)
+            {
+                throw new NotImplementedException();
+            }
+
+            public object ReadFromStream(Stream stream)
+            {
+                throw new NotImplementedException();
+            }
+
+            public IEnumerable<MediaTypeHeaderValue> SupportedMediaTypes
+            {
+                get
+                {
+                    return new[]
+                               {
+                                   new MediaTypeHeaderValue("application/xml"),
+                                   new MediaTypeHeaderValue("text/html")
+                               };
+                }
+            }
         }
     }
 }
