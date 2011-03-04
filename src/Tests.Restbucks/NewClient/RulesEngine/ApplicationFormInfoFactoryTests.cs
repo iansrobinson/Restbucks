6b34acd3bb510751f9773f2f335502557046fa7a
iansrobinson <iansrobinson@d29beace-2619-7b4a-91c4-328ad5d2ee20>
2011-03-04 17:05:23 +0000

    Added some new tests
    git-svn-id: file:///Users/iansrobinson/Development/svn-repos/Restbucks.Procurement@402 d29beace-2619-7b4a-91c4-328ad5d2ee20

---
+++ src/Tests.Restbucks/NewClient/RulesEngine/ApplicationFormInfoFactoryTests.cs
@@ -0,0 +1,28 @@
+﻿using System.Net.Http;
+using NUnit.Framework;
+using Restbucks.Client.Formatters;
+using Restbucks.NewClient.RulesEngine;
+using Rhino.Mocks;
+
+namespace Tests.Restbucks.NewClient.RulesEngine
+{
+    [TestFixture]
+    public class ApplicationFormInfoFactoryTests
+    {
+        [Test]
+        public void ShouldCallStrategyWithSuppliedContextAndContentAdapter()
+        {
+            var response = new HttpResponseMessage();
+            var context = new ApplicationContext(new object());
+            var adapter = new HttpContentAdapter(RestbucksMediaTypeFormatter.Instance);
+
+            var strategy = MockRepository.GenerateMock<IFormStrategy>();
+            strategy.Expect(s => s.GetFormInfo(response, context, adapter)).Return(null);
+
+            var factory = new ApplicationFormInfoFactory(strategy, context, adapter);
+            factory.CreateFormInfo(response);
+
+            strategy.VerifyAllExpectations();
+        }
+    }
+}
