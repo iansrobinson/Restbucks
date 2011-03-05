6cead7f49b838b76a184b0b5ce38f9393700d192
iansrobinson <iansrobinson@d29beace-2619-7b4a-91c4-328ad5d2ee20>
2011-03-05 12:43:06 +0000

    Ensured Restbucks links are always formatted as absolute URIs
    git-svn-id: file:///Users/iansrobinson/Development/svn-repos/Restbucks.Procurement@406 d29beace-2619-7b4a-91c4-328ad5d2ee20

--- src/Restbucks.NewClient/RestbucksLink.cs
+++ src/Restbucks.NewClient/RestbucksLink.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using Restbucks.MediaType;
@@ -22,8 +23,8 @@ namespace Restbucks.NewClient
 
         public LinkInfo GetLinkInfo(HttpResponseMessage response, HttpContentAdapter contentAdapter)
         {
-            var entityBody = contentAdapter.CreateObject(response.Content);
-            var link = (from l in ((Shop) entityBody).Links
+            var entityBody = (Shop)contentAdapter.CreateObject(response.Content);
+            var link = (from l in (entityBody).Links
                         where l.Rels.Contains(relation, LinkRelationEqualityComparer.Instance)
                         select l).FirstOrDefault();
 
@@ -32,7 +33,9 @@ namespace Restbucks.NewClient
                 throw new ControlNotFoundException(string.Format("Could not find link with link relation '{0}'.", relation.Value));
             }
 
-            return new LinkInfo(link.Href, new MediaTypeHeaderValue(link.MediaType));
+            var resourceUri = link.Href.IsAbsoluteUri ? link.Href : new Uri(entityBody.BaseUri, link.Href);
+
+            return new LinkInfo(resourceUri, new MediaTypeHeaderValue(link.MediaType));
         }
     }
 }
---
+++ src/Tests.Restbucks/NewClient/RestbucksLinkTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using NUnit.Framework;
+using Restbucks.Client.Formatters;
+using Restbucks.MediaType;
+using Restbucks.NewClient;
+using Restbucks.NewClient.RulesEngine;
+
+namespace Tests.Restbucks.NewClient
+{
+    [TestFixture]
+    public class RestbucksLinkTests
+    {
+        [Test]
+        public void ShouldConvertRelativeUrisToAbsoluteUris()
+        {
+            var contentAdapter = new HttpContentAdapter(RestbucksMediaTypeFormatter.Instance);
+
+            var entityBody = new ShopBuilder(new Uri("http://localhost/virtual-directory/"))
+                .AddLink(new Link(
+                             new Uri("request-for-quote", UriKind.Relative),
+                             RestbucksMediaType.Value,
+                             new StringLinkRelation("rfq")))
+                .Build();
+
+            var link = new RestbucksLink(new StringLinkRelation("rfq"));
+            var linkInfo = link.GetLinkInfo(new HttpResponseMessage {Content = contentAdapter.CreateContent(entityBody, new MediaTypeHeaderValue(RestbucksMediaType.Value))}, contentAdapter);
+
+            Assert.AreEqual(new Uri("http://localhost/virtual-directory/request-for-quote"), linkInfo.ResourceUri);
+        }
+    }
+}
