32d94b69f33e7a97d4e369302df7bc0cd91eec15
iansrobinson <iansrobinson@d29beace-2619-7b4a-91c4-328ad5d2ee20>
2011-03-18 10:31:32 +0000

    Completed the IsXXX sattus code extension methods
    git-svn-id: file:///Users/iansrobinson/Development/svn-repos/Restbucks.Procurement@466 d29beace-2619-7b4a-91c4-328ad5d2ee20

--- src/Tests.Restbucks/Client/Http/HttpStatusCodeExtensionMethodsTests.cs
+++ src/Tests.Restbucks/Client/Http/HttpStatusCodeExtensionMethodsTests.cs
@@ -8,6 +8,15 @@ namespace Tests.Restbucks.Client.Http
     public class HttpStatusCodeExtensionMethodsTests
     {
         [Test]
+        public void Is1XXTests()
+        {
+            Assert.IsTrue(HttpStatusCode.Continue.Is1XX());
+            Assert.IsTrue(HttpStatusCode.SwitchingProtocols.Is1XX());
+
+            Assert.IsFalse(HttpStatusCode.OK.Is1XX());
+        }
+
+        [Test]
         public void Is2XXTests()
         {
             Assert.IsTrue(HttpStatusCode.OK.Is2XX());
@@ -15,6 +24,35 @@ namespace Tests.Restbucks.Client.Http
 
             Assert.IsFalse(HttpStatusCode.Continue.Is2XX());
             Assert.IsFalse(HttpStatusCode.SeeOther.Is2XX());
+        }
+
+        [Test]
+        public void Is3XXTests()
+        {
+            Assert.IsTrue(HttpStatusCode.MultipleChoices.Is3XX());
+            Assert.IsTrue(HttpStatusCode.TemporaryRedirect.Is3XX());
+
+            Assert.IsFalse(HttpStatusCode.OK.Is3XX());
+            Assert.IsFalse(HttpStatusCode.NotFound.Is3XX());
+        }
+
+        [Test]
+        public void Is4XXTests()
+        {
+            Assert.IsTrue(HttpStatusCode.BadRequest.Is4XX());
+            Assert.IsTrue(HttpStatusCode.ExpectationFailed.Is4XX());
+
+            Assert.IsFalse(HttpStatusCode.MultipleChoices.Is4XX());
+            Assert.IsFalse(HttpStatusCode.InternalServerError.Is4XX());
+        }
+
+        [Test]
+        public void Is5XXTests()
+        {
+            Assert.IsTrue(HttpStatusCode.InternalServerError.Is5XX());
+            Assert.IsTrue(HttpStatusCode.HttpVersionNotSupported.Is5XX());
+
+            Assert.IsFalse(HttpStatusCode.BadRequest.Is5XX());
         }
     }
 }
